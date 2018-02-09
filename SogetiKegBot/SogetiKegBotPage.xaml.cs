9a4620ecb7c32be6e4889a0a6ad963846d5852a0
mkenar10 <mkenar10@gmail.com>
2018-02-09 14:20:00 -0600

    Initial check-in of module SogetiKegBot

---
+++ SogetiKegBot/SogetiKegBotPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SogetiKegBot
+{
+    public partial class SogetiKegBotPage : ContentPage
+    {
+        public SogetiKegBotPage()
+        {
+            InitializeComponent();
+
+            image.Source = ImageSource.FromFile("SogetiKegBot.Droid/Resources/drawable/spade.jpg");
+
+            //var btn = this.FindByName<Button>("button");
+        }
+
+        public void pourBeer(object sender, EventArgs e){
+            DisplayAlert("Alert","Here is some beer!","OK");
+        }
+    }
+}
