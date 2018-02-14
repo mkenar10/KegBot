10dad44b01b11995d6c9eaa60e9e52ddae067d64
mkenar10 <mkenar10@gmail.com>
2018-02-14 09:26:27 -0600

    Added images for logo and beer. Beer image is clickable and displays pop-up.

--- SogetiKegBot/SogetiKegBotPage.xaml.cs
+++ SogetiKegBot/SogetiKegBotPage.xaml.cs
@@ -9,13 +9,27 @@ namespace SogetiKegBot
         {
             InitializeComponent();
 
-            image.Source = ImageSource.FromFile("SogetiKegBot.Droid/Resources/drawable/spade.jpg");
+            //images
+            logo.Source = ImageSource.FromFile("SogetiKegBot.Droid/Resources/drawable/logo.jpg");
+            img.Source = ImageSource.FromFile("SogetiKegBot.Droid/Resources/drawable/guinness.jpg");
 
-            //var btn = this.FindByName<Button>("button");
+            //labels
+            ontap.FontAttributes = FontAttributes.Italic;
+            ontap.FontSize = 24;
+
+            //onTap for Image
+            var tapImage = new TapGestureRecognizer();
+            tapImage.Tapped += tapImage_Tapped;
+            img.GestureRecognizers.Add(tapImage);
+
+            //add measurement of how much poured here.
+            void tapImage_Tapped(object sender, EventArgs e){
+                DisplayAlert("Alert","Your pour: ","OK");
+            }
         }
 
-        public void pourBeer(object sender, EventArgs e){
-            DisplayAlert("Alert","Here is some beer!","OK");
-        }
+        //public void pourBeer(object sender, EventArgs e){
+           // DisplayAlert("Alert","Here is some beer!","OK");
+        //}
     }
 }
